af0c52c3ca66d96c6dc81b6ec11abee3adfda0c2
ilaiyaraja1 <ilaiyaraja.bca@gmail.com>
2021-08-23 21:58:05 +0530

    specific day to date
    if your want weekdays specific date to date .you can use this method...

--- src/days/Dates.java
+++ src/days/Dates.java
@@ -1,17 +1,23 @@
 package days;
 
+import java.util.EnumSet;
+
 public class Dates {
          
 	static enum day{monday,tuesday,wednesday,thursday,friday,saturday,sunday};
 	
-	
 	public static void main(String[] args) {
-          
 		System.out.println("your beautiful day is:"+day.monday);
 		
-         for(day d:day.values()){         //day d:day.values()...
+         for(day d:day.values()){         //day d:day.values()...      
         	 System.out.println("day:"+d);
+         }
+         //then if you whant spcific day to day you should use this method
+         System.out.println("************************************************");
+         
+         for(day days:EnumSet.range(day.monday,day.friday)){  // Enumset.range(date to date).
         	 
+        	 System.out.println("your secand day is:"+days);
          }
 		
 		}
